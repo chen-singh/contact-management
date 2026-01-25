3c7e7f64496c10761e3aba63292bf33d291fee70
chen-singh <cssodho123@gmail.com>
2026-01-25 22:17:11 +0400

    updating files

--- backend/contact/src/main/java/com/main/contact/service/UserDetailServiceImpl.java
+++ backend/contact/src/main/java/com/main/contact/service/UserDetailServiceImpl.java
@@ -1,29 +1,28 @@
-//package com.main.contact.service;
+package com.main.contact.service;
 //
-//import com.main.contact.entity.User;
-//import com.main.contact.repository.UserInterface;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Component;
-//import org.springframework.stereotype.Service;
-//
+import com.main.contact.entity.User;
+import com.main.contact.entity.UserPrincipal;
+import com.main.contact.repository.UserInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
 //@Component
-//@Service
-//public class UserDetailServiceImpl implements UserDetailsService {
-//
-//    @Autowired
-//    private UserInterface userrepo;
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user=userrepo.findByEmail(username);
-//        if (user!=null){
-//            org.springframework.security.core.userdetails.User.builder()
-//                    .username(user.getEmail())
-//                    .password(user.getPassword()).build();
-//        }
-//        throw new UsernameNotFoundException("User not found"+username);
-//
-//    }
-//}
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserInterface userrepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userrepo.findByEmail(username);
+        if (user==null){
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new UserPrincipal();
+    }
+}
