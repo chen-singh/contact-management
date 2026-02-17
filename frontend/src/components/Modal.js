b59e6408fc40915b20e88af6df225f9b70b7308c
chen-singh <cssodho123@gmail.com>
2026-02-17 22:20:44 +0400

    Udated files

---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Modal = ({ title, children, show, onClose, footer }) => {
+  if (!show) return null;
+
+  return (
+    <>
+      {/* Backdrop */}
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          zIndex: 1040,
+        }}
+        onClick={onClose} // close modal if backdrop clicked
+      ></div>
+
+      {/* Modal container */}
+      <div
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          zIndex: 1050,
+          width: "100%",
+          maxWidth: "500px",
+          backgroundColor:"lightgray",
+          borderRadius:"5%",
+          padding:"5px"
+
+        }}
+      >
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{title}</h5>
+            <button
+              type="button"
+              className="btn-close"
+              onClick={onClose}
+            ></button>
+          </div>
+
+          <div className="modal-body">{children}</div>
+
+          {footer && <div className="modal-footer">{footer}</div>}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
