b74d2bf690bc2ab0f52646cfb82525c346ef31df
chen-singh <cssodho123@gmail.com>
2026-03-01 09:42:24 +0400

    Udated files

--- backend/CMS/src/main/java/in/cs/main/mapper/ContactSpecification.java
+++ backend/CMS/src/main/java/in/cs/main/mapper/ContactSpecification.java
@@ -35,65 +35,25 @@ public class ContactSpecification {
                 );
             }
 
-            // If both are present → use OR
+
             if (firstNamePredicate != null && lastNamePredicate != null) {
                 return cb.or(firstNamePredicate, lastNamePredicate);
             }
 
-            // If only firstName
+
             if (firstNamePredicate != null) {
                 return firstNamePredicate;
             }
 
-            // If only lastName
+
             if (lastNamePredicate != null) {
                 return lastNamePredicate;
             }
 
-            // If nothing provided → return all
+            
             return cb.conjunction();
         };
     }
 
-//    public static Specification<Contact> filterBy(
-//            String firstName,
-//            String lastName,
-//            Pageable pageable
-//    ) {
-//
-//        return (Root<Contact> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
-//
-//            query.distinct(true);
-//
-//            Predicate predicate = cb.conjunction();
-//
-//            if (firstName != null && !firstName.isBlank()) {
-//                predicate = cb.and(predicate,
-//                        cb.like(cb.lower(root.get("firstName")),
-//                                "%" + firstName.toLowerCase() + "%"));
-//            }
-//
-//            if (lastName != null && !lastName.isBlank()) {
-//                predicate = cb.and(predicate,
-//                        cb.like(cb.lower(root.get("lastName")),
-//                                "%" + lastName.toLowerCase() + "%"));
-//            }
-//
-////            if (email != null && !email.isBlank()) {
-////                Join<Object, Object> emailJoin = root.join("emails", JoinType.LEFT);
-////                predicate = cb.and(predicate,
-////                        cb.like(cb.lower(emailJoin.get("emailAddress")),
-////                                "%" + email.toLowerCase() + "%"));
-////            }
-//
-////            if (phone != null && !phone.isBlank()) {
-////                Join<Object, Object> phoneJoin = root.join("phones", JoinType.LEFT);
-////                predicate = cb.and(predicate,
-////                        cb.like(phoneJoin.get("phoneNumber"),
-////                                "%" + phone + "%"));
-////            }
-//
-//            return predicate;
-//        };
-//    }
+
 }
