6f3ba1050e709281268804124c90203306692420
chen-singh <163499959+chen-singh@users.noreply.github.com>
2026-03-09 10:12:18 +0400

    Clean up ContactService.js by removing comments
    Remove commented-out code for API calls.

--- frontend/src/components/ContactService.js
+++ frontend/src/components/ContactService.js
@@ -1,18 +1,1 @@
 
-// import axios from "axios";
-
-// const API_URL = "http://localhost:8080/api/contacts";
-
-// export const getContacts = (page = 0, size = 10) => {
-//   return axios.get(`${API_URL}?page=${page}&size=${size}`);
-// };
-
-// export const createContact = (contact) => {
-//   return axios.post(API_URL, contact);
-// };
-
-// export const searchByName = (firstName, lastName) => {
-//   return axios.get(`${API_URL}/search`, {
-//     params: { firstName, lastName }
-//   });
-// };
